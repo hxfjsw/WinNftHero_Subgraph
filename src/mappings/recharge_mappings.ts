5d76c89b1f770b7101330f28ad05001e26d86517
rodulph.huang <rudolph.huang@tron.network>
2022-06-16 21:13:57 +0800

    init

---
+++ src/mappings/recharge_mappings.ts
@@ -0,0 +1,35 @@
+import {BigInt} from "@graphprotocol/graph-ts"
+import {
+    Recharge as RechargeEvent,
+    OwnershipTransferred,
+    Paused,
+    Unpaused
+} from "../../generated/RechargeContract/RechargePay"
+import { Recharge} from "../../generated/schema"
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+}
+
+export function handlePaused(event: Paused): void {
+}
+
+export function handleRecharge(event: RechargeEvent): void {
+
+    let entity = Recharge.load(event.transaction.from.toHex())
+
+    if (!entity) {
+        entity = new Recharge(event.transaction.from.toHex())
+    }
+
+    entity.from = event.params.from;
+    entity.to = event.params.to;
+    entity.amount = event.params.amount;
+    entity.order_id = event.params.order_id;
+    entity.token = event.params.token_address;
+    entity.timestamp = event.block.timestamp;
+
+    entity.save()
+}
+
+export function handleUnpaused(event: Unpaused): void {
+}
